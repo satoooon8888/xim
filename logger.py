7b52e9acb5ee10963e2d37d609c169ea5db67c91
satoooon8888 <samidare256@gmail.com>
2020-03-30 22:05:59 +0900

    refactor logging.py and rename logging.py to logger.py

---
+++ logger.py
@@ -0,0 +1,10 @@
+def error(text: str) -> None:
+	print("Error: ".format(text))
+
+
+def warning(text: str) -> None:
+	print("Warn: {}".format(text))
+
+
+def info(text: str) -> None:
+	print("Xim: {}".format(text))
