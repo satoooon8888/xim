0baef0fded4241c88ba429a10e8f0e4518698d17
satoooon8888 <samidare256@gmail.com>
2020-03-31 18:02:50 +0900

    fix bug

--- commands/current.py
+++ commands/current.py
@@ -1,12 +1,12 @@
 from config import ConfigJSONFileStream, Config
-from const_setting import config_path
+from const_setting import config_path, un_setting_current_proxy
 
 
 def current(args) -> None:
 	stream = ConfigJSONFileStream(config_path)
 	config: Config = stream.load()
 	current_proxy: str = config["current_proxy"]
-	if current_proxy == "__un_setting_":
+	if current_proxy == un_setting_current_proxy:
 		print("haven't set proxy yet.")
 		return
 	print("current set proxy: {}".format(current_proxy))
--- const_setting.py
+++ const_setting.py
@@ -3,5 +3,6 @@ from proxy import Proxies, Proxy
 
 config_path: str = "./xim.config.json"
 proxies_path: str = "./xim_proxies.json"
-config_default: Config = {"current_proxy": "__un_setting__"}
+un_setting_current_proxy = "__un_setting__"
+config_default: Config = {"current_proxy": un_setting_current_proxy}
 proxies_default: Proxies = Proxies([Proxy("noproxy", "", "", "", "")])
