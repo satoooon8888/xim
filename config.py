d48136a32b7cef8e0f7ba260b5ef3c8a591b6611
satoooon8888 <samidare256@gmail.com>
2020-03-31 17:02:00 +0900

    impl config

---
+++ config.py
@@ -0,0 +1,20 @@
+import json
+from typing import TypedDict
+
+
+class Config(TypedDict):
+	current_proxy: str
+
+
+class ConfigJSONFileStream:
+	def __init__(self, path: str) -> None:
+		self.path = path
+
+	def load(self) -> Config:
+		with open(self.path, "rt") as f:
+			config: Config = json.load(f)
+		return config
+
+	def save(self, config: Config) -> None:
+		with open(self.path, "wt") as f:
+			json.dump(config, f)
